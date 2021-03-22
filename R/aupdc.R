ba4a67e9ccf0e2319037e53b1e4857b50f333d90
joaobtj <joaobtj@gmail.com>
2021-03-22 14:33:47 -0300

    update audpc

--- R/aupdc.R
+++ R/aupdc.R
@@ -1,9 +1,9 @@
-#' AUPDC
+#' AUDPC
 #'
 #' @param y Ammount of the disease.
 #' @param time Measurement time
 #'
-#' @return area Area under progress disease curve
+#' @return area Area under the disease progress curve
 #'
 #' @examples
 #' day <- seq(10,60,10)
@@ -11,7 +11,7 @@
 #' aacpd(y=sev, time=day)
 
 #' @export
-aupdc <- function(y, time) {
+audpc <- function(y, time) {
   area <- 0
   for (i in 1:(length(y) - 1)) {
     area = area + (y[i + 1] + y[i]) / 2 * (time[i + 1] - time[i])
