dc8f8e2dea835c0f5e8226dcdf776b7c845587ec
joaobtj <joao.tolentino@ufsc.br>
2022-08-30 15:05:25 -0300

    mvi

--- R/mvi.R
+++ R/mvi.R
@@ -7,15 +7,17 @@
 #'
 #' @examples
 #' day <- seq(10, 60, 10)
-#' sev <- c(0.22, 0.26, 0.28, 0.36, 0.39, 0.42)
-#' plot(sev~day)
+#' sev <- c(0.22, NA, 0.28, 0.36, 0.39, 0.42)
+#' plot(sev ~ day)
 #' mvi(y = sev, time = day)
 #' @export
 mvi <- function(y, time) {
   mvi <- 0
-  for (i in 1:(length(y) - 1)) {
-    mvi <- mvi + (y[i + 1] - y[i]) /  (time[i + 1] - time[i])
+  for (i in seq.int(length(y) - 1)) {
+    if (!is.na((y[i + 1] - y[i]) / (time[i + 1] - time[i]))) {
+      mvi <- mvi + (y[i + 1] - y[i]) / (time[i + 1] - time[i])
+    }
   }
 
-  return(mvi/(length(y) - 1))
+  return(mvi / (length(y) - 1))
 }
